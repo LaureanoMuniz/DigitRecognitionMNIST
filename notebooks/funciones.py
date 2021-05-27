c75a553e856d94d437380bd7bc590ead5bafa0e0
Laureano <lmuniz@dc.uba.ar>
2021-05-26 23:15:52 -0300

    Actualización en funciones.py

--- notebooks/funciones.py
+++ notebooks/funciones.py
@@ -5,6 +5,7 @@ import numpy as np
 import sklearn as sk
 from sklearn import metrics
 import time
+import metnum
 
 def construir_dataset(numeroElementos = 42000):
 	df_train = pd.read_csv("../data/train.csv")
@@ -15,11 +16,10 @@ def construir_dataset(numeroElementos = 42000):
 	# Uso values para mandar todo a arrays de numpy
 	X_test = df_test[df_test.columns[1:]].values
 	y_test = df_test["label"].values.reshape(-1, 1)
- 
- 	df_train = df_train[:min(numeroElementos,cutting_point)]
+	df_train = df_train[:min(numeroElementos,cutting_point)]
 	X = df_train[df_train.columns[1:]].values
 	y = df_train["label"].values.reshape(-1, 1)
-	return(X, Y, X_test, y_test)
+	return(X, y, X_test, y_test)
 
 def get_KFold_sets(x,y,K=5):
     X_trains = []
@@ -60,23 +60,23 @@ def encontrarParOptimo(X_trains,Y_trains,X_vals,Y_vals,conPeso=False):
 	accspar.sort(reverse = True)
 	mejores5pares = accspar[:5]
 	minimosf1 = minimizarF1(mejores5pares,X_trains,Y_trains,X_vals,Y_vals,conPeso)
-	return minimosf1[0][1]
+	return minimosf1[0]
 
 def minimizarF1(mejores5,X_trains,Y_trains,X_vals,Y_vals,conPeso=False):
 	minimosf1 = []
 	digitosConPeoresF1 = []
-	for tupla in mejores5pares:
+	for tupla in mejores5:
 		f1_scorePorClase = np.zeros(10)
 		for i in range(len(X_trains)):
-			pca = metnum.PCA(tupla[2])
-			pca.fit(X_trains[i])
-			X_train_transformada = pca.transform(X_trains[i])
-			X_val_transformada = pca.transform(X_vals[i])
+		    pca = metnum.PCA(tupla[2])
+		    pca.fit(X_trains[i])
+		    X_train_transformada = pca.transform(X_trains[i])
+		    X_val_transformada = pca.transform(X_vals[i])
 		    clf = metnum.KNNClassifier(tupla[1])
 		    clf.fit(X_train_transformada, Y_trains[i])
 		    y_pred = clf.predict(X_val_transformada)
-		    f1_scorePorClase += sk.metrics.f1_score(Y_vals[i],y_pred, average = None)
-		f1_scorePorClase = f1_scorePorClase/10
-		minimosf1.append(np.amin(f1_scorePorClase),(tupla[1],tupla[2]))
+		    f1_scorePorClase += np.array(sk.metrics.f1_score(Y_vals[i],y_pred, average = None))
+		f1_scorePorClase = f1_scorePorClase/len(X_trains)
+		minimosf1.append((np.amin(f1_scorePorClase),tupla[0],tupla[1],tupla[2]))
 	minimosf1.sort(reverse = True)
 	return(minimosf1)
